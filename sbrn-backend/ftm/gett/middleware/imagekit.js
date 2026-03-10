b50fbfe13d6af4b6fc3ff85aeedca498bf39786c
mustafa20170986 <jster0986@gmail.com>
2026-03-10 08:35:55 -0700

    unwanted removed

--- sbrn-backend/ftm/gett/middleware/imagekit.js
+++
@@ -1,9 +0,0 @@
-// SDK initialization
-
-import ImageKit from "imagekit"
-
- export const imagekit = new ImageKit({
-    publicKey : "public_2IeVUftkbzKaxhdJyKPlsnKlZWc=",
-    privateKey : "private_7OF2HcxY8+sNHQdoTsGYmIgMKeg=",
-    urlEndpoint : "https://ik.imagekit.io/zuqazdd5w"
-});
--- sbrn-backend/ftm/gett/prisma/migrations/20260129071415_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ALTER COLUMN "imageurl" DROP NOT NULL;
